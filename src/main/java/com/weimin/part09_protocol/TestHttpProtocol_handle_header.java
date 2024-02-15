56426da876fb6766b095a67b6f814c3a25ffc995
weimin <2561394933@qq.com>
2024-02-15 15:58:39 +0800

    netty 处理 http协议

--- src/main/java/com/weimin/part09_protocol/TestHttpProtocol_handle_header.java
+++ src/main/java/com/weimin/part09_protocol/TestHttpProtocol_handle_header.java
@@ -2,19 +2,16 @@ package com.weimin.part09_protocol;
 
 import com.weimin.Logger;
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelInitializer;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.http.HttpContent;
-import io.netty.handler.codec.http.HttpRequest;
-import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.*;
 
-public class TestHttpProtocol {
-    private static final Logger logger = new Logger(TestHttpProtocol.class);
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_LENGTH;
+
+public class TestHttpProtocol_handle_header {
+    private static final Logger logger = new Logger(TestHttpProtocol_handle_header.class);
 
     public static void main(String[] args) {
 
@@ -29,24 +26,22 @@ public class TestHttpProtocol {
                 protected void initChannel(SocketChannel socketChannel) throws Exception {
                     //socketChannel.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
                     socketChannel.pipeline().addLast(new HttpServerCodec()); // http协议的编解码器
-                    socketChannel.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+
+                    // SimpleChannelInboundHandler，只关心处理请求头和请求行
+                    socketChannel.pipeline().addLast(new SimpleChannelInboundHandler<HttpRequest>() {
                         @Override
-                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                            logger.info("收到客户端的消息：");
-                            logger.info(msg.toString());
-                            logger.info("消息类型：");
-                            logger.info(msg.getClass().toString());
+                        protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpRequest httpRequest) throws Exception {
+                            logger.info(httpRequest.uri());
 
-                            // 客户端发了一次请求，但是 HttpServerCodec 解码器 将请求分成两个部分处理
-                            // 第一部分是请求头和请求行
-                            // 第二部分是请求体
+                            DefaultFullHttpResponse response =
+                                    new DefaultFullHttpResponse(httpRequest.protocolVersion(), HttpResponseStatus.OK);
 
-                            if(msg instanceof HttpRequest){
-                                // 处理请求头和请求行
-                            }else if(msg instanceof HttpContent){
-                                // 处理请求体
-                            }
+                            byte[] msg = "<h1>Hello, I'm server!</h1>".getBytes();
+                            response.content().writeBytes(msg);
+                            // 告诉浏览器响应数据的长度，防止浏览器一直等待
+                            response.headers().setInt(CONTENT_LENGTH, msg.length);
 
+                            channelHandlerContext.writeAndFlush(response);
                         }
                     });
                 }
