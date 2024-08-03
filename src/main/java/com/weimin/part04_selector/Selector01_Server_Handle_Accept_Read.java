a833260d62bbcc7896c2199b1c8ddcf9abadbdf4
weimin <2561394933@qq.com>
2024-08-03 20:49:46 +0800

    补充注释

--- src/main/java/com/weimin/part04_selector/Selector01_Server_Handle_Accept_Read.java
+++ src/main/java/com/weimin/part04_selector/Selector01_Server_Handle_Accept_Read.java
@@ -49,14 +49,19 @@ public class Selector01_Server_Handle_Accept_Read {
 
 
         while (true) {
-            // 没有事件发生时，线程会阻塞
+            // 没有事件发生时，线程会阻塞，也就是select方法会在没有事件发生时阻塞线程
             // 有事件，线程才会恢复运行
             // 事件未处理时，select也不会阻塞
             // 事件发生后，要么处理，要么取消，不能置之不理，否则事件会一直存在，
             selector.select();
 
+            // 当运行到select方法之后，说明有事件发生；
+
             // 处理事件
-            // 得到所有SelectionKey的集合
+            // 得到所有SelectionKey的集合（事件集合）
+            // 当某个channel上有事件发生时，就会有这么一个SelectionKey对象被创建，代表某个channel上发生了某一事件
+            // selector管理多个channel，这里就拿到所有发生了事件的channel对应的SelectionKey
+            // 然后遍历处理
             Set<SelectionKey> selectionKeys = selector.selectedKeys();
             Iterator<SelectionKey> iterator = selectionKeys.iterator();
             while (iterator.hasNext()) {
@@ -69,6 +74,7 @@ public class Selector01_Server_Handle_Accept_Read {
                     SocketChannel socketChannel = channel.accept();
                     socketChannel.configureBlocking(false);
 
+                    // 每个channel使用单独的buffer
                     ByteBuffer buffer = ByteBuffer.allocate(16);
                     socketChannel.register(selector, SelectionKey.OP_READ, buffer);
                 } else if (selectionKey.isReadable()) {
@@ -76,6 +82,8 @@ public class Selector01_Server_Handle_Accept_Read {
                         SocketChannel channel = (SocketChannel) selectionKey.channel();
                         ByteBuffer buffer = (ByteBuffer) selectionKey.attachment();
                         int read = channel.read(buffer);
+
+                        // read等于-1，代表客户端正常断开
                         if (read == -1) {
                             selectionKey.cancel();
                         } else {
@@ -92,6 +100,7 @@ public class Selector01_Server_Handle_Accept_Read {
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
+                        // 发生异常，代表客户端强制断开
                         selectionKey.cancel();
                     }
                 }
